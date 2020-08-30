22069a4583b2f6059eff0537dd30dbc494f4f438
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-08-30 12:09:06 -0300

    SOLID & Services

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 // parseISO irá converter uma data em formato de string para
 // o formato Date() do JS
 
 // startOfHour vai pegar uma data e colocar o minuto,seg,miliseg como 0
 
+// Rota: Receber a req, chamar outro arquivo, devolver resposta
+// Se a rota está fazendo mais coisas, precisa ser abstraido
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
@@ -17,26 +20,24 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate,
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
