4865357610aebb329fb7aebec6f8af768639243d
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-09-05 22:50:16 -0300

    repository with typeORM

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,40 +1,19 @@
-import { isEqual } from 'date-fns';
+import { Repository, EntityRepository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 
-// DTO = Data Transfer Object
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
+// Appointment é o model
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
   // Para Ler,buscar,criar ou alterar a entidade apenas
   // esse repositorio deve ser capaz de fazer isso
 
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
     return findAppointment || null;
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-
-    return appointment;
   }
 }
 
