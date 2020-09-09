a14e02434f914624eb0eaaca5723b7006f14ec35
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-09-09 00:14:46 -0300

    errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 // import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -10,6 +12,24 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  // Verificar se o erro é uma instancia da classe appErrror
+  // Se for, quer dizer que foi um erro originado da nossa aplicação
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
