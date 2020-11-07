b6f37787992fa39c5fc50177f2dc009f76a2d95a
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-11-07 16:23:38 -0300

    configuring imports

--- src/modules/appointments/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/repositories/AppointmentsRepository.ts
@@ -1,6 +1,6 @@
 import { Repository, EntityRepository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 
 // Appointment é o model
 @EntityRepository(Appointment)
