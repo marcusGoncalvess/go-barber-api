fa081ba50672593101fe81e4feaaf3d69a8aa123
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-11-08 01:50:21 -0300

    dependency inversion principle

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,35 +1,30 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 
 import { parseISO } from 'date-fns';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-// parseISO irá converter uma data em formato de string para
-// o formato Date() do JS
 
-// startOfHour vai pegar uma data e colocar o minuto,seg,miliseg como 0
-
-// Rota: Receber a req, chamar outro arquivo, devolver resposta
-// Se a rota está fazendo mais coisas, precisa ser abstraido
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
