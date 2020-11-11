e3590b2f4356968fd7af57d2a2b93c828a3f7d1d
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-11-11 19:34:19 -0300

    dependency injection

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,4 @@
-// import 'reflect-metadata';
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
@@ -8,6 +8,7 @@ import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
