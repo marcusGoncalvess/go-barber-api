43c1a1bb48ac38e1d14956f3ae9018f85c8a4603
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2020-12-02 00:21:06 -0300

    listing providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,7 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -10,21 +8,19 @@ interface IRequest {
 }
 
 @injectable()
-class ShowProfileService {
+class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
   ) {}
 
-  public async execute({ user_id }: IRequest): Promise<User> {
-    const user = await this.usersRepository.findById(user_id);
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
 
-    if (!user) {
-      throw new AppError('User not found.');
-    }
-
-    return user;
+    return users;
   }
 }
 
-export default ShowProfileService;
+export default ListProvidersService;
