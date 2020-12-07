cfea2c89a8a4d9490560cb35d031a453cef79a29
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2020-12-07 01:49:13 -0300

    emails with amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'vini4705@gmail.com',
+      name: 'Marcus Gonçalves',
+    },
+  },
+} as IMailConfig;
